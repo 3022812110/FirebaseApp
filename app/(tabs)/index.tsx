9f6cf2900b9f09ab9d1ed11ca019827c4e815f53
zhangmi <3022812110@qq.com>
2024-12-15 16:49:44 +0800

    显示选中图片

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,6 +9,7 @@ import IconButton from "../components/IconButton";
 import CircleButton from "../components/CircleButton";
 import EmojiPicker from "../components/EmojiPicker";
 import EmojiList from "../components/EmojiList";
+import EmojiSticker from "../components/EmojiSticker";
 
 
 const PlaceholderImage = require('../../assets/images/background-image.png');
@@ -60,6 +61,7 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.iamgeContainer}>
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
+        {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
